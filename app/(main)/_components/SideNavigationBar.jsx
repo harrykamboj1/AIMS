e95e3088708fb8a796091c186663348dc56b06cf
harrykamboj1 <singhharnoor116@gmail.com>
2024-07-25 23:00:51 +0530

    SideBar Navigation

--- app/(main)/_components/SideNavigationBar.jsx
+++ app/(main)/_components/SideNavigationBar.jsx
@@ -1,24 +1,61 @@
-import { BadgeIcon, BookIcon } from "lucide-react";
+"use client";
+import { useTheme } from "next-themes";
+import { BadgeCheck, BadgeIcon, BookIcon, BookOpenIcon } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const menuData = [
   {
     id: 1,
     name: "All Courses",
-    icon: BookIcon,
+    icon: BookOpenIcon,
   },
   {
     id: 2,
     name: "Membership",
-    icon: BadgeIcon,
+    icon: BadgeCheck,
   },
 ];
 
 const SideNavigationBar = () => {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
-    <div className="">
-      <Image src={"/AIMS.png"} height={50} width={50} />
+    <div className="bg-white p-5 shadow-sm border h-screen">
+      {theme === "dark" && (
+        <Image src={"/AIMS.png"} height={50} width={50} alt="logo" shadow-md />
+      )}
+      {theme === "light" && (
+        <Image
+          src={"/AIMS_LIGHT.png"}
+          height={80}
+          width={170}
+          alt="logo"
+          shadow-md
+        />
+      )}
+      {theme === "system" && (
+        <Image src={"/AIMS.png"} height={50} width={50} alt="logo" shadow-md />
+      )}
+
+      <div className="mt-7">
+        {menuData.map((item, index) => (
+          <div
+            className="group flex gap-3 mt-1 p-3 text-[16px] items-center text-gray-900 cursor-pointer hover:bg-primary hover:text-white rounded-md transition-all"
+            key={index}
+          >
+            <item.icon className="group-hover:animate-bounce" />
+            <h2>{item.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -3,11 +3,11 @@ import SideNavigationBar from './_components/SideNavigationBar'
 
 function layout({ children }) {
     return (
-        <div className='grid'>
-            <div className=' bg-red-400 sm:w-64 hidden sm:block grid-cols-3 fixed'>
+        <div >
+            <div className=' sm:w-64 hidden sm:block fixed'>
                 <SideNavigationBar />
             </div>
-            <div className='sm:ml-64 grid-cols-9 bg-blue-400 w-full'>
+            <div className='sm:ml-64'>
                 {children}
             </div>
         </div>
