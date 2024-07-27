7a7f6c6d54975504b7fdfca80a613e97ad8fb06b
harrykamboj1 <singhharnoor116@gmail.com>
2024-07-27 15:21:41 +0530

    Banner added

--- app/(main)/courses/page.js
+++ app/(main)/courses/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import WelcomeBanner from '../_components/WelcomeBanner'
 
 function Cousese() {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className='grid grid-cols-1 md:grid-cols-3 p-5'>
             <div className='col-span-2'>
-                Left
+                <WelcomeBanner />
             </div>
             <div>
                 Right
